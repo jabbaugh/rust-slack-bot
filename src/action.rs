cae81bfd9b2569823529f2d057f90df5e447ddcd
Jim Baugh <jim.baugh@oracle.com>
2020-06-24 13:45:19 -0600

    latest

--- src/action.rs
+++ src/action.rs
@@ -5,7 +5,7 @@ use self::slack::RtmClient;
 pub fn respond_hi(bot_id: &str, text: &str, channel: &str, cli: &RtmClient) {
     let pattern = format!("<@{}> {}", bot_id, "hi");
 
-    if text.contains(&pattern) {
-        let _ = cli.sender().send_message(channel, "Hi!");
-    }
+//    if text.contains(&pattern) {
+        let _ = cli.sender().send_message(channel, format!("{}!", text.to_string()));
+//    }
 }
--- src/handler.rs
+++ src/handler.rs
@@ -8,7 +8,7 @@ pub struct Handler;
 #[allow(unused_variables)]
 impl EventHandler for Handler {
     fn on_event(&mut self, cli: &RtmClient, event: Event) {
-        println!("on_event(event: {:?})", event);
+//        println!("on_event(event: {:?})", event);
 
         match event.clone() {
             Event::Message(message) => self.handle_message(*message, cli, &event),
@@ -28,24 +28,35 @@ impl EventHandler for Handler {
 #[allow(unused_variables)]
 impl Handler {
     fn handle_message(&mut self, message: Message, cli: &RtmClient, event: &Event) {
+        let bot_name = "rustbot";
         let message_standard = match message {
             Message::Standard(message_standard) => message_standard,
             _ => return
         };
+        let exp_channel = "DA03FJJH3";
+        let exp_user = "W6B1UEMEF";
         let channel: String = message_standard.channel.unwrap();
+        let user: String = message_standard.user.unwrap();
 
         let bot_id: &str = cli.start_response().slf.as_ref().unwrap().id.as_ref().unwrap();
-        if &message_standard.user.unwrap() == bot_id {
+        println!("Bot ID: {} | channel: {} | user: {}", bot_id, channel, user);
+        if user == bot_id {
             println!("Is own message");
             return
         }
 
         let text: String = message_standard.text.unwrap();
-        if !text.contains(bot_id) {
-            println!("Is not a mention");
+
+        if !user.eq(exp_user) {
+            println!("Is not right user");
             return
         }
 
-        action::respond_hi(&bot_id, &text, &channel, &cli);
+        if text.contains(bot_name) {
+            println!("say hi");
+            action::respond_hi(&bot_id, &text, &channel, &cli);
+        }
+
+        return
     }
 }
